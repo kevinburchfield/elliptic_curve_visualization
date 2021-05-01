963177ea0cbdb79e14181f2ec25475017e9ed2f0
Kevin <kevinburchfi@gmail.com>
2021-05-01 02:00:21 -0400

    Point selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,60 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Graph from './components/graph';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <Graph width={750} height={750}></Graph>
-      </header>
-    </div>
-  );
+const App = () => {
+    let [chosenPoints, setChosenPoints] = useState<{x: number, y: number}[]>([]);
+
+    const choosePoint = (point: {x: number, y: number}) => {
+        setChosenPoints(prev => {
+            if (prev.some(p => p.x === point.x && p.y === point.y)) {
+                return prev;
+            }
+            if (!!prev[1]) {
+                return [prev[1], point];
+            } else if (!!prev[0]) {
+                return [prev[0], point];
+            } else {
+                return [point];
+            }
+        });
+    };
+
+    useEffect(() => {
+
+    }, [chosenPoints]);
+
+    return (
+        <div className="App">
+            <div className="Info">
+                <p>
+                    This is a tool to visualize Elliptic Curve cryptography.
+                </p>
+                <p>
+                    The strength of Elliptic Curve cryptography is in the fact that it is very easy to perform N actions on an EC for a given starting point to find an ending point, but is extremely difficult to figure out N given those two points.
+                </p>
+                <p>
+                    Try selecting a number of actions, as well as two base points (for simplicity), to begin simulating EC cryptography and find your public key!
+                </p>
+                {chosenPoints.length >= 1 &&
+                    <p>
+                        <b>Point A:</b> ({chosenPoints[0].x}, {chosenPoints[0].y})
+                    </p>
+                }
+                {chosenPoints.length === 2 &&
+                    <>
+                        <p>
+                            <b>Point B:</b> ({chosenPoints[1].x}, {chosenPoints[1].y})
+                        </p>
+                        <button>Simulate!</button>
+                    </>
+                }
+            </div>
+            <header className="App-header">
+                <Graph width={750} height={750} choosePoint={choosePoint}/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -1,73 +1,55 @@
 import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
+import "./graph.css";
 
 // https://observablehq.com/@d3/connected-scatterplot
 
-const Graph = (props: { width: number, height: number }) => {
+const Graph = (props: { width: number, height: number, choosePoint: Function }) => {
     // Graph attributes
     const { width, height } = props;
     const domainMultiplier = 150;
     const margin = 30;
     const opacity = 0.1;
     const graphTransitionDuration = 5000;
-    const plotTransitionDuration = 2500;
+    const plotTransitionDuration = 500;
+    const basePointSize = 2;
+    const pointModifier = 3;
+    let chosenPoints: any[] = [];
 
     const generateTestData = (a?: number, b?: number): { x: number, y: number }[] => {
         // Elliptic Curve formula : y^2 = x^3 + ax + b
         // Test with a = 0, b = 1, over the range of
         // const constants = { a:0, b: 0 };
-        const constantA = !!a ? a : -2;
-        const constantB = !!b ? b : -1;
-        console.log(constantA);
-        console.log(constantB);
+        const constantA = !!a ? a : -1;
+        const constantB = !!b ? b : 1;
         const points: { x: number, y: number }[] = [];
-        for(let x = -3; x <= 3; x+=0.003) {
+        for (let x = -3; x <= 3; x += 0.003) {
             let pointX = parseFloat(x.toPrecision(10));
-            let y = pointX**3 + (constantA*pointX) + constantB;
-            y = y > 0 ? y**.5 : -(Math.abs(y)**.5);
-            if (y > 0){
-                points.push({ x, y });
-                points.push({ x, y: -y });
+            let y = pointX ** 3 + (constantA * pointX) + constantB;
+            y = y > 0 ? y ** .5 : -(Math.abs(y) ** .5);
+            if (y > 0) {
+                points.push({ x: +x.toFixed(3), y: +y.toFixed(3) });
+                points.push({ x: +x.toFixed(3), y: -(+y.toFixed(3)) });
             }
         }
         return points;
-    }
+    };
 
-    const renderGraph = async (animateAxis: boolean = false) => {
+    const renderGraph = (animateAxis: boolean = false) => {
         let svg: any;
         if (!svgElement) {
             svg = d3.select(ref.current);
             setSvgElement(svg);
-            console.log("1234");
         } else {
             svg = svgElement;
             svg.selectAll("*").remove();
-            console.log("asdf");
-            setDataset(generateTestData(1, 1));
+            setDataset(() => generateTestData(1, 1));
         }
-
-        console.log(dataset);
 
         drawXAxis(svg, animateAxis);
         drawYAxis(svg, animateAxis);
         drawPlotArea(svg);
-        dataset.forEach((p, i) => drawCircle(svg, p, i));
-    }
-
-    const addPoints = (svg: any, data: { x: number, y: number }[]) => {
-        svg.append("g")
-            .attr("id", "plotArea")
-            .attr("width", width)
-            .attr("fill", "white")
-            .attr("stroke", "black")
-            .attr("stroke-width", 0)
-            .selectAll("circle")
-            .data(data)
-            .join("circle")
-            .attr("cx", (d: any) => x(d.x))
-            .attr("cy", (d: any) => y(d.y))
-            .attr("r", 1);
-    }
+    };
 
     const drawPlotArea = (svg: any) => {
         svg.append("g")
@@ -75,21 +57,68 @@ const Graph = (props: { width: number, height: number }) => {
             .attr("fill", "white")
             .attr("stroke", "black")
             .attr("stroke-width", 0);
-    }
+    };
 
-    const drawCircle = (svg: any, point: {x: number, y: number}, delay: number) => {
+    const drawCircle = (svg: any, point: { x: number, y: number }, delay: number) => {
         svg.selectAll("#plotArea")
             .append("circle")
             .data([point])
             .join("circle")
+            .on('mouseover', function(d: any, i: any) {
+                // @ts-ignore
+                d3.select(this)
+                    .transition()
+                    .duration(5)
+                    .attr('r', basePointSize + pointModifier)
+                    .attr('fill', '#f48024');
+                // @ts-ignore
+                d3.select(this)
+                    .raise();
+            })
+            .on('mouseout', function(d: any, i: any) {
+                if (chosenPoints.some(p => p.x === point.x && p.y === point.y)) {
+                    return;
+                }
+                // @ts-ignore
+                d3.select(this)
+                    .transition()
+                    .duration(5)
+                    .attr('r', basePointSize)
+                    .attr('fill', '#FFF')
+            })
+            .on('click', function(d: any, i: any) {
+                props.choosePoint(i);
+                if (!!chosenPoints[1]) {
+                    d3.select(`[idx='${chosenPoints[0].x}'][idy='${chosenPoints[0].y}']`)
+                        .transition()
+                        .duration(5)
+                        .attr('r', basePointSize)
+                        .attr('fill', '#FFF');
+
+                    chosenPoints = [chosenPoints[1], point];
+                } else if (!!chosenPoints[0]) {
+                    chosenPoints = [chosenPoints[0], point];
+                } else {
+                    chosenPoints = [point];
+                }
+
+                // @ts-ignore
+                d3.select(this)
+                    .transition()
+                    .duration(5)
+                    .attr('r', basePointSize + pointModifier)
+                    .attr('fill', '#ff0101');
+            })
             .attr("cx", (d: any) => x(d.x))
             .attr("cy", (d: any) => y(d.y))
             .attr("r", 0)
+            .attr('idx', (d: any) => d.x)
+            .attr('idy', (d: any) => d.y)
             .transition()
             .delay(delay)
             .duration(plotTransitionDuration)
-            .attr("r", 1.5);
-    }
+            .attr("r", basePointSize);
+    };
 
     const drawXAxis = (svg: any, animateAxis: boolean) => {
         svg.append("g")
@@ -113,7 +142,7 @@ const Graph = (props: { width: number, height: number }) => {
                 .duration(graphTransitionDuration)
                 .attr("stroke-dashoffset", 0)
             )
-    }
+    };
     const x = d3.scaleLinear()
         .domain([-(width / domainMultiplier), width / domainMultiplier])
         .range([margin, width - margin]);
@@ -143,21 +172,28 @@ const Graph = (props: { width: number, height: number }) => {
                 .duration(graphTransitionDuration)
                 .attr("stroke-dashoffset", 0)
             )
-    }
+    };
 
     // D3
     const ref = useRef(null);
-    const [dataset, setDataset] = useState<{x: number, y: number}[]>(() => generateTestData());
+    const [dataset, setDataset] = useState<{ x: number, y: number }[]>(() => generateTestData());
     const [svgElement, setSvgElement] = useState<any>();
+    const [tooltipElement, setTooltipElement] = useState<any>();
 
     useEffect(() => {
         renderGraph(true);
     }, []);
 
+    useEffect(() => {
+        dataset.forEach((p, i) => drawCircle(d3.select(ref.current), p, i));
+    }, [dataset]);
+
     return (
         <div>
-            <svg className="container" width={width} height={height} ref={ref}/>
-            <button onClick={() => renderGraph(false)}>Render Graph</button>
+            <svg className="container" width={width} height={height} ref={ref} />
+            <div className="Control-Container">
+                <button onClick={() => renderGraph(false)}>Render Graph</button>
+            </div>
         </div>
     );
 }
